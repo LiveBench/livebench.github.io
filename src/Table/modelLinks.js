0d84d5b3df8c423eae92fbcde2b201a4356802c7
Gabriel Guralnick <gabriel@abacus.ai>
2025-11-17 10:49:38 -0600

    adding results

--- src/Table/modelLinks.js
+++ src/Table/modelLinks.js
@@ -204,4 +204,11 @@ export const modelLinks = {
     "deepseek-v3.2-exp": {url: "https://huggingface.co/deepseek-ai/DeepSeek-V3.2-Exp", organization: "DeepSeek", displayName: "DeepSeek V3.2 Exp", version: "2025-10-17", openweight: true},
     "deepseek-v3.2-exp-thinking": {url: "https://huggingface.co/deepseek-ai/DeepSeek-V3.2-Exp", organization: "DeepSeek", displayName: "DeepSeek V3.2 Exp Thinking", version: "2025-10-17", openweight: true, reasoner: true},
     "minimax-m2": {url: "https://huggingface.co/MiniMaxAI/MiniMax-M2", organization: "Minimax", displayName: "Minimax M2", version: "2025-10-29", openweight: true, reasoner: true},
+    "kimi-k2-thinking": {url: "https://huggingface.co/moonshotai/Kimi-K2-Thinking", organization: "Moonshot AI", displayName: "Kimi K2 Thinking", version: "2025-11-08", openweight: true, reasoner: true},
+    "grok-4-fast-non-reasoning-2511": {url: "https://docs.x.ai/docs/models/grok-4-fast-non-reasoning", organization: "xAI", displayName: "Grok 4 Fast (Non-Reasoning)", reasoner: false, version: "2025-11-10"},
+    "grok-4-fast-reasoning-2511": {url: "https://docs.x.ai/docs/models/grok-4-fast-reasoning", organization: "xAI", displayName: "Grok 4 Fast", reasoner: true, version: "2025-11-10"},
+    "gpt-5.1-2025-11-13-high": {url: "https://openai.com/index/gpt-5-1/", organization: "OpenAI", displayName: "GPT-5.1 High", version: "2025-11-13", reasoner: true},
+    "gpt-5.1-2025-11-13-nothinking": {url: "https://openai.com/index/gpt-5-1/", organization: "OpenAI", displayName: "GPT-5.1 No Thinking", version: "2025-11-13", reasoner: false},
+    "gpt-5.1-codex": {url: "https://platform.openai.com/docs/models/gpt-5.1-codex", organization: "OpenAI", displayName: "GPT-5.1 Codex", version: "2025-11-13", reasoner: true},
+    "gpt-5.1-codex-mini": {url: "https://platform.openai.com/docs/models/gpt-5.1-codex-mini", organization: "OpenAI", displayName: "GPT-5.1 Codex Mini", version: "2025-11-13", reasoner: true},
 };
